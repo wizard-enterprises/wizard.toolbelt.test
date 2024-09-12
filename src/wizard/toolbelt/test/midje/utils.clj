39b72c691e5e0ef12ea8a1580bdad74aad83a7a5
Lectia Landau <lectia@wizard.enterprises>
2024-09-12 19:17:36 +0300

    remove unused functions

--- src/wizard/toolbelt/test/midje/utils.clj
+++ src/wizard/toolbelt/test/midje/utils.clj
@@ -1,42 +1,5 @@
 (ns wizard.toolbelt.test.midje.utils
-  (:use midje.sweet tupelo.core)
-  (:require [potemkin :refer [unify-gensyms]]))
-
-(defmacro thread-list
-  [thread x & exprs]
-  (unify-gensyms
-   `(let [x## ~x]
-      (list ~@(map (fn [e] `(~thread x## ~e)) exprs)))))
-
-(defmacro list->>
-  [x & exprs]
-  `(thread-list ->> ~x ~@exprs))
-
-(defmacro list->
-  [x & exprs]
-  `(thread-list -> ~x ~@exprs))
-
-(defmacro thread-context-list
-  [thread x & exprs]
-  `(context (str '~x)
-     (thread-list ~thread ~x ~@exprs)))
-
-(defmacro context->>
-  [x & exprs]
-  `(thread-context-list ->> ~x ~@exprs))
-
-(defmacro context->
-  [x & exprs]
-  `(thread-context-list -> ~x ~@exprs))
-
-(defmacro thread-it-list
-  [thread x message & exprs]
-  `(it (str '~x " " '~message)
-     (thread-list ~thread ~x ~@exprs)))
-
-(defmacro it->>
-  [x message & exprs]
-  `(thread-it-list ->> ~x ~message ~@exprs))
+  (:use fudje.sweet tupelo.core))
 
 (defn- assert-ex-info-message
   [message]
